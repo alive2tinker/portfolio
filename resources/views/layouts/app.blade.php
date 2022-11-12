187fd362c81722b376e3463963f13b584341f733
Abdulmalik Alsufayran <alsufiran@gmail.com>
2022-11-12 12:42:09 +0300

    updated seo settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>{{ config('app.name', 'laravel') }}</title>
+        <meta name="description" content="I am Abdulmalik Alsufayran, Software developer and amateur SCRUM Master / Product Owner" />
+        <meta name="keywords" content="laravel,tailwind,alpinejs,livewire,inertiajs,flutter,html,css,javascript,ui,ux,design,scrum" />
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
