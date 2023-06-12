024352f1f0538a4bd1a97da8896bcb7ade289001
Abdulmalik Alsufayran <abdulmalik.alsufayra@sa.zain.com>
2023-06-12 14:48:36 +0300

    update project logo

--- resources/views/livewire/projects-page.blade.php
+++ resources/views/livewire/projects-page.blade.php
@@ -8,31 +8,46 @@
         </div>
         <div class="grid py-7 sm:grid-cols-3 gap-4">
             @foreach($user->projects as $project)
-            <div>
-                    <div class="rounded-full w-16 h-16 shadow-md overflow-hidden p-4" >
-                    <img class="" src="{{ asset('storage/' . $project->icon ) }}" alt="">
+                <div>
+                    <div class="rounded-full w-16 h-16 shadow-md overflow-hidden p-4 flex">
+                        @if($project->icon)
+                            <img class="" src="{{ asset('storage/' . $project->icon ) }}" alt="">
+                        @else
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 m-auto">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"/>
+                            </svg>
+                        @endif
                     </div>
                     <h2 class="mt-6 text-xl font-bold text-zinc-800 dark:text-zinc-100">
-                        {{ $project->title }} @if(!$project->live) <span class="rounded-full text-sm my-2 bg-red-100 border border-red-500 px-4 text-red-500 font-semibold">{{ __('Soon') }}</span> @endif
+                        {{ $project->title }} @if(!$project->live)
+                            <span
+                                class="rounded-full text-sm my-2 bg-red-100 border border-red-500 px-4 text-red-500 font-semibold">{{ __('Soon') }}</span>
+                        @endif
                     </h2>
                     <p class="text-zinc-800 py-5 dark:text-zinc-400">{{ Str::limit(strip_tags($project->excerpt), 100, '...') }}</p>
                     <div class="flex space-x-2 space-y-2 w-auto">
-                    <div class="flex-1">
-                    @foreach($project->technologies as $technology)
-                    <span class="inline-flex items-center rounded-full bg-teal-100 px-2.5 py-0.5 text-xs font-medium text-teal-800">{{ $technology['name']}}</span>
-                        @endforeach
-                    </div>
+                        <div class="flex-1">
+                            @foreach($project->technologies as $technology)
+                                <span
+                                    class="inline-flex items-center rounded-full bg-teal-100 px-2.5 py-0.5 text-xs font-medium text-teal-800">{{ $technology['name']}}</span>
+                            @endforeach
+                        </div>
                     </div>
                     @if($project->link)
-                    <div class="flex space-x-4 py-5">
-                        <a class="text-zinc-800 dark:text-zinc-400" href="{{ $project->link }}">{{ $project->link }}</a>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 dark:text-zinc-400">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
-</svg>
-                    </div>
+                        <div class="flex space-x-4 py-5">
+                            <a class="text-zinc-800 dark:text-zinc-400"
+                               href="{{ $project->link }}">{{ $project->link }}</a>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                 stroke="currentColor" class="w-6 h-6 dark:text-zinc-400">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                      d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"/>
+                            </svg>
+                        </div>
                     @endif
 
-            </div>
+                </div>
             @endforeach
         </div>
     </div>
