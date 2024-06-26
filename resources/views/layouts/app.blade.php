cecae276388b3680703e8cd91b63cab7bccf5cdb
Abdulmalik Salman Fahad Alsufayran <alsufiran@gmail.com>
2024-06-26 22:34:57 +0300

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
     <link rel="canonical" href="https://www.malikthefullstack.com">
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-    <!-- <link href="{{ asset('js-libs/splide/dist/css/splide.min.css') }}" rel="stylesheet" />
-    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script> -->
+    <link href="{{ asset('js-libs/splide/dist/css/splide.min.css') }}" rel="stylesheet" />
+    <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.3/dist/js/splide.min.js"></script>
     <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('favicon/apple-touch-icon.png') }}">
     <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon/favicon-32x32.png') }}">
     <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon/favicon-16x16.png') }}">
