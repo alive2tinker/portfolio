54042fd039df0311bc551778c872afafde769e0e
Abdulmalik.Alsufayra <abdulmalik.alsufayra@sa.zain.com>
2024-10-15 21:56:23 +0300

    hot fix

--- routes/web.php
+++ routes/web.php
@@ -18,13 +18,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function(){
-    return View('welcome');
-});
+
 Route::group(['prefix' => LaravelLocalization::setLocale()], function()
 {
     /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
-    Route::get('/home', \App\Livewire\HomePage::class)->name('home-page');
+    Route::get(uri: '/', \App\Livewire\HomePage::class)->name('home-page');
     Route::get('/about', AboutPage::class)->name('about');
     Route::get('/projects', ProjectsPage::class)->name('projects');
     Route::get('/articles', ArticlesPage::class)->name('articles');
