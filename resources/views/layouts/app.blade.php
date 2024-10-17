da5864cc9c6cb36e6fb5c4ba3512b632469c76d3
abdulmalik alsufayran <alsufiran@gmail.com>
2024-10-17 04:06:18 +0300

    corrected favicon source

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -38,9 +38,9 @@
         integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
 
     <!-- FAVICON -->
-    <link rel="icon" type="image/png" href="/favicon-48x48.png" sizes="48x48" />
-    <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="icon" type="image/png" href="{{ asset("favicon/favicon-48x48.png") }}" sizes="48x48" />
+    <link rel="icon" type="image/svg+xml" href="{{ asset("favicon/favicon.svg") }}" />
+    <link rel="shortcut icon" href="{{ asset('favicon/favicon.ico') }}" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
     <meta name="apple-mobile-web-app-title" content="Malikthefullstack" />
     <link rel="manifest" href="{{ asset('favicon/site.webmanifest') }}" />
