6c76aff782289317b04be7bf6e1c0d37802df88b
abdulmalik alsufayran <alsufiran@gmail.com>
2024-10-23 23:32:16 +0300

    caching navigation

--- app/Livewire/Footer.php
+++ app/Livewire/Footer.php
@@ -4,6 +4,8 @@ namespace App\Livewire;
 
 use App\Models\MenuLink;
 use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
 use Livewire\Component;
 
 class Footer extends Component
@@ -12,15 +14,17 @@ class Footer extends Component
     public $links;
     public function mount()
     {
-        $this->links = MenuLink::where([
-            ['menu_link_id',null],
-            ['location', 'bottom'],
-            ['user_id', 1],
-        ])->orWhere([
-            ['menu_link_id',null],
-            ['location', 'both'],
-            ['user_id', 1],
-        ])->get();
+        $this->links = Cache::remember('menu-links', Carbon::now()->addMonth(), function(){
+            return MenuLink::where([
+                ['menu_link_id',null],
+                ['location', 'bottom'],
+                ['user_id', 1],
+            ])->orWhere([
+                ['menu_link_id',null],
+                ['location', 'both'],
+                ['user_id', 1],
+            ])->get();
+        });
 
         $this->user = User::find(1);
     }
